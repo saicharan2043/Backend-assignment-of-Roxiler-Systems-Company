1bee5f2fa2363e6cfaab61be7863af235fa585c6
saicharan2043 <saikann2043@gmail.com>
2023-11-12 18:13:21 +0530

    added frontend url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://product-transction-project.netlify.app/",
+      "https://product-transction-project.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
