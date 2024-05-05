0d86d7d56999cf3dcf221e1843c34755a985919b
saicharan2043 <saikann2043@gmail.com>
2024-05-06 01:21:28 +0530

    changed database connection and code

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const mysql = require("mysql");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 
-const connection = mysql.createConnection({
-  host: "db4free.net",
-  port: 3306,
-  user: "shiva_kumar",
-  password: "sai@1234",
-  database: "user_details_db",
-});
-
-connection.connect(function (error) {
-  if (error) throw error;
-  console.log("database successfuly connected..");
-});
+const databasePath = path.join(__dirname, "Transactions.db");
 
 dotenv.config({
   path: "./data/config.env",
@@ -23,184 +14,165 @@ dotenv.config({
 
 app.use(cors());
 
+let db;
+const initializeDatabaseAndServer = async () => {
+  try {
+    db = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(process.env.PORT, () =>
+      console.log(`server is running on port ${process.env.PORT}`)
+    );
+  } catch (e) {
+    console.log(`Database connect error ${e}`);
+  }
+};
+
+initializeDatabaseAndServer();
+
 app.get("/getdata", async (request, response) => {
-  const dataResponse = await fetch(
-    "https://s3.amazonaws.com/roxiler.com/product_transaction.json"
-  );
-  const data = await dataResponse.json();
-  console.log(data.length);
-  //   for (let eachValue of data) {
-  //     connection.query(
-  //       `
-  //     insert into Product(
-  //         title , price , description, category , image , sold , dateOfSale
-  //     )values ('${eachValue.title}' , ${eachValue.price} , '${eachValue.description}' , "${eachValue.category}" , '${eachValue.image}' , ${eachValue.sold} , '${eachValue.dateOfSale}')
-  //     `,
-  //       (error, reslut) => {
-  //         if (error) throw error;
-  //         console.log(reslut);
-  //       }
-  //     );
-  //   }
-  response.send("data sended");
-});
-
-app.get("/getTableTransactions", (request, response) => {
-  const { search, month, pageno, limit } = request.query;
-  const skip = (pageno - 1) * limit;
-  const checkSearchValue = search === "0" ? "%%" : `%${search}%`;
-  connection.query(
-    `
-    select * from Product where (title Like '${checkSearchValue}' or description Like '${checkSearchValue}' or price Like '${checkSearchValue}') and MONTH(dateOfSale) LIKE ${month} limit ${limit} offset ${skip}
-  `,
-    (error, reslut) => {
-      if (error) throw error;
-      response.send(reslut);
+  try {
+    const dataResponse = await fetch(
+      "https://s3.amazonaws.com/roxiler.com/product_transaction.json"
+    );
+    const data = await dataResponse.json();
+    for (let eachValue of data) {
+      const query = `
+      insert into Product(
+          title , price , description, category , image , sold , dateOfSale
+      )values ('${eachValue.title}' , ${eachValue.price} , '${eachValue.description}' , "${eachValue.category}" , '${eachValue.image}' , ${eachValue.sold} , '${eachValue.dateOfSale}')
+      `;
+      await db.run(query);
     }
-  );
+    response.status(200);
+    response.send({ msg: "data is inserted successfuly" });
+  } catch (e) {
+    response.status(400);
+    response.send({ error: `something went to wrong please try agian` });
+  }
 });
 
-app.get("/getStatistics", (request, response) => {
-  const { month } = request.query;
-  const query1 = `select sum(price) as sumOf from Product where MONTH(dateOfSale) LIKE ${month} and sold = 1`;
-  const query2 = `select count(*) as numSoldOf from Product where MONTH(dateOfSale) LIKE ${month} and sold = 1`;
-  const query3 = `select count(*) as numUnsoldOf from Product where MONTH(dateOfSale) LIKE ${month} and sold = 0`;
-  // request one
-  connection.query(query1, (error1, sumOfReslut) => {
-    if (error1) throw error1;
-    // request two
-    connection.query(query2, (error2, numberSoldOfReslut) => {
-      if (error2) throw error2;
-      // request three
-      connection.query(query3, (error3, numberUnsoldOfReslut) => {
-        if (error3) throw error3;
-        // send response to the client
-        response.send({
-          sumOfReslut: sumOfReslut[0].sumOf,
-          numberSoldOfReslut: numberSoldOfReslut[0].numSoldOf,
-          numberUnsoldOfReslut: numberUnsoldOfReslut[0].numUnsoldOf,
-        });
-      });
+app.get("/getTableTransactions", async (req, rep) => {
+  try {
+    const { search, month, pageno, limit } = req.query;
+    const skip = (pageno - 1) * limit;
+    const query = `select * from Product where (title Like '%${search}%' or description Like '%${search}%' or price Like '%${search}%') and strftime("%m" , dateOfSale) LIKE '${month}' limit ${limit} offset ${skip}`;
+    const response = await db.all(query);
+    rep.status(200);
+    rep.send(response);
+  } catch (e) {
+    rep.status(400);
+    rep.send({ error: `something went to wrong please try agian` });
+    console.log(e);
+  }
+});
+
+app.get("/getStatistics", async (req, rep) => {
+  try {
+    const { month } = req.query;
+    const queryOfSum = `select cast(sum(price) as int) as sumOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and sold = 1`;
+    const queryOfCountOfSold = `select count(*) as numSoldOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and sold = 1`;
+    const queryOfCountOfNoneSold = `select count(*) as numUnsoldOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and sold = 0`;
+    const responseOfSum = await db.get(queryOfSum);
+    const responseOfCountOfSold = await db.get(queryOfCountOfSold);
+    const responseOfCountOfNoneSold = await db.get(queryOfCountOfNoneSold);
+    rep.status(200);
+    rep.send({
+      sumOfReslut: responseOfSum.sumOf,
+      numberSoldOfReslut: responseOfCountOfSold.numSoldOf,
+      numberUnsoldOfReslut: responseOfCountOfNoneSold.numUnsoldOf,
     });
-  });
+  } catch (e) {
+    rep.status(400);
+    rep.send({ error: `something went to wrong please try agian` });
+  }
 });
 
-app.get("/barChart", (request, response) => {
-  const { month } = request.query;
-  const query1 = `select count(*) as 0To100 from Product where MONTH(dateOfSale) LIKE ${month} and price between 0 and 100`;
-  const query2 = `select count(*) as 101To200 from Product where MONTH(dateOfSale) LIKE ${month} and price between 101 and 200`;
-  const query3 = `select count(*) as 201To300 from Product where MONTH(dateOfSale) LIKE ${month} and price between 201 and 300`;
-  const query4 = `select count(*) as 301To400 from Product where MONTH(dateOfSale) LIKE ${month} and price between 301 and 400`;
-  const query5 = `select count(*) as 401To500 from Product where MONTH(dateOfSale) LIKE ${month} and price between 401 and 500`;
-  const query6 = `select count(*) as 501To600 from Product where MONTH(dateOfSale) LIKE ${month} and price between 501 and 600`;
-  const query7 = `select count(*) as 601To700 from Product where MONTH(dateOfSale) LIKE ${month} and price between 601 and 700`;
-  const query8 = `select count(*) as 701To800 from Product where MONTH(dateOfSale) LIKE ${month} and price between 701 and 800`;
-  const query9 = `select count(*) as 801To900 from Product where MONTH(dateOfSale) LIKE ${month} and price between 801 and 900`;
-  const query10 = `select count(*) as 901ToAbove from Product where MONTH(dateOfSale) LIKE ${month} and price >= 901`;
+app.get("/barChart", async (req, rep) => {
+  try {
+    const { month } = req.query;
+    const query1 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 0 and 100`;
+    const query2 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 101 and 200`;
+    const query3 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 201 and 300`;
+    const query4 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 301 and 400`;
+    const query5 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 401 and 500`;
+    const query6 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 501 and 600`;
+    const query7 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 601 and 700`;
+    const query8 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 701 and 800`;
+    const query9 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price between 801 and 900`;
+    const query10 = `select count(*) as countOf from Product where strftime("%m" , dateOfSale) LIKE '${month}' and price >= 901`;
 
-  // query one
-  connection.query(query1, (error1, reslutOf0To100) => {
-    if (error1) throw error1;
-    // query two
-    connection.query(query2, (error2, reslutOf101To200) => {
-      if (error2) throw error2;
-      // query three
-      connection.query(query3, (error3, reslutOf201To300) => {
-        if (error3) throw error3;
-        // query four
-        connection.query(query4, (error4, reslutOf301To400) => {
-          if (error4) throw error4;
-          // query five
-          connection.query(query5, (error5, reslutOf401To500) => {
-            if (error5) throw error5;
-            // query six
-            connection.query(query6, (error6, reslutOf501To600) => {
-              if (error6) throw error6;
-              // query seven
-              connection.query(query7, (error7, reslutOf601To700) => {
-                if (error7) throw error7;
-                // query eight
-                connection.query(query8, (error8, reslutOf701To800) => {
-                  if (error8) throw error8;
-                  // query nine
-                  connection.query(query9, (error9, reslutOf801To900) => {
-                    if (error9) throw error9;
-                    // query ten
-                    connection.query(query10, (error10, reslutOf901ToAbove) => {
-                      if (error10) throw error10;
-                      // send response
-                      // response.send({
-                      //   reslutOf0To100: reslutOf0To100[0]["0To100"],
-                      //   reslutOf101To200: reslutOf101To200[0]["101To200"],
-                      //   reslutOf201To300: reslutOf201To300[0]["201To300"],
-                      //   reslutOf301To400: reslutOf301To400[0]["301To400"],
-                      //   reslutOf401To500: reslutOf401To500[0]["401To500"],
-                      //   reslutOf501To600: reslutOf501To600[0]["501To600"],
-                      //   reslutOf601To700: reslutOf601To700[0]["601To700"],
-                      //   reslutOf701To800: reslutOf701To800[0]["701To800"],
-                      //   reslutOf801To900: reslutOf801To900[0]["801To900"],
-                      //   reslutOf901ToAbove: reslutOf901ToAbove[0]["901ToAbove"],
-                      // });
-                      response.send([
-                        { name: "0-100", countOf: reslutOf0To100[0]["0To100"] },
-                        {
-                          name: "101-200",
-                          countOf: reslutOf101To200[0]["101To200"],
-                        },
-                        {
-                          name: "201-300",
-                          countOf: reslutOf201To300[0]["201To300"],
-                        },
-                        {
-                          name: "301-400",
-                          countOf: reslutOf301To400[0]["301To400"],
-                        },
-                        {
-                          name: "401-500",
-                          countOf: reslutOf401To500[0]["401To500"],
-                        },
-                        {
-                          name: "501-600",
-                          countOf: reslutOf501To600[0]["501To600"],
-                        },
-                        {
-                          name: "601-700",
-                          countOf: reslutOf601To700[0]["601To700"],
-                        },
-                        {
-                          name: "701-800",
-                          countOf: reslutOf701To800[0]["701To800"],
-                        },
-                        {
-                          name: "801-900",
-                          countOf: reslutOf801To900[0]["801To900"],
-                        },
-                        {
-                          name: "901-above",
-                          countOf: reslutOf901ToAbove[0]["901ToAbove"],
-                        },
-                      ]);
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
+    const respons1 = await db.get(query1);
+    const respons2 = await db.get(query2);
+    const respons3 = await db.get(query3);
+    const respons4 = await db.get(query4);
+    const respons5 = await db.get(query5);
+    const respons6 = await db.get(query6);
+    const respons7 = await db.get(query7);
+    const respons8 = await db.get(query8);
+    const respons9 = await db.get(query9);
+    const respons10 = await db.get(query10);
+    rep.status(200);
+    rep.send([
+      { name: "0-100", countOf: respons1["countOf"] },
+      {
+        name: "101-200",
+        countOf: respons2["countOf"],
+      },
+      {
+        name: "201-300",
+        countOf: respons3["countOf"],
+      },
+      {
+        name: "301-400",
+        countOf: respons4["countOf"],
+      },
+      {
+        name: "401-500",
+        countOf: respons5["countOf"],
+      },
+      {
+        name: "501-600",
+        countOf: respons6["countOf"],
+      },
+      {
+        name: "601-700",
+        countOf: respons7["countOf"],
+      },
+      {
+        name: "701-800",
+        countOf: respons8["countOf"],
+      },
+      {
+        name: "801-900",
+        countOf: respons9["countOf"],
+      },
+      {
+        name: "901-above",
+        countOf: respons10["countOf"],
+      },
+    ]);
+  } catch (e) {
+    rep.status(400);
+    rep.send({ error: `something went to wrong please try agian` });
+    console.log(e);
+  }
 });
 
-app.get("/pieChart", (request, response) => {
-  const { month } = request.query;
-  const query = `select category , count(*) as ItemsCount from Product where MONTH(dateOfSale) LIKE ${month} group by category`;
-  connection.query(query, (error, reslut) => {
-    if (error) throw error;
-    response.send(reslut);
-  });
+app.get("/pieChart", async (req, rep) => {
+  try {
+    const { month } = req.query;
+    const query = `select category , count(*) as ItemsCount from Product where strftime("%m" , dateOfSale) LIKE '${month}' group by category`;
+    const response = await db.all(query);
+    rep.status(200);
+    rep.send(response);
+  } catch (e) {
+    rep.status(400);
+    rep.send({ error: `something went to wrong please try agian` });
+  }
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`server is running ${process.env.port}`)
-);
+app.get("/", (req, res) => {
+  res.send({ msg: "hello" });
+});
